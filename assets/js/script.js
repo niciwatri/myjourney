8525bf0861b630da5fd9e319e9251bf62d683c9e
Nicole Watrinet <nicolewatrinet@HFIAD17-nicole-watrinet.local>
2018-01-13 08:26:23 +0100

    Daten hinzugefügt

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+$(document).foundation()
+
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= $(window).height()) {
+        $(".Ebenen").addClass(".fixed");
+    } else {
+        $(".Ebenen").removeClass(".fixed");
+    }
+});
