cbda3bba5d2e8e882ab367bd774e01aff3af8513
Nicole Watrinet <nicolewatrinet@HFIAD17-nicole-watrinet.local>
2018-01-19 14:47:36 +0100

    Stand vor Stefan

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,60 @@
 $(document).foundation()
 
-$(window).scroll(function() {    
+$(window).scroll(function() {   
+
     var scroll = $(window).scrollTop();
 
-    if (scroll >= $(window).height()) {
-        $(".Ebenen").addClass("fixed");
-    } else {
-        $(".Ebenen").removeClass("fixed");
-    }
+    $(".Ebenen").each(
+   		function(index, element){
+           	var offsetTop = $(element).offset().top;
+
+
+           if (scroll >= offsetTop) {
+               //$(".fixed").html($(element).html());
+               //$(element).html().insertAfter( "#fixed .inner" );
+               //$(element).css('opacity',0);
+               //$(element).addClass("fixed");
+               if(element.id == "Leute"){
+               		$("#Leute").addClass("fixed");
+               }
+               else if(element.id == "Tram"){
+               		$("#Tram").addClass("fixed");
+               }
+               else if(element.id == "Video"){
+               		$("#Video").addClass("fixed");
+               }
+
+           } else {
+               $(".Ebenen").removeClass("fixed");
+           }
+		}
+    );
+  	
+
+
+    // if (scroll >= $(window).height()) {
+    //     $(".Ebenen").addClass("fixed");
+    // } else {
+    //     $(".Ebenen").removeClass("fixed");
+    // }
 });
+
+
+
+
+
+//    $(".Ebenen").each(
+//        function(index, element){
+//            var offsetTop = $(element).offset().top;
+
+//            if (scroll >= offsetTop) {
+//                //$(".fixed").html($(element).html());
+//                $(element).html().insertAfter( "#fixed .inner" );
+//                $(element).css('opacity',0);
+//                //$(element).addClass("fixed");
+//            } else {
+//                //$(".Ebenen").removeClass("fixed");
+//            }
+
+//        }
+//    );
